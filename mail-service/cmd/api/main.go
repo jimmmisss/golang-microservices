019215b2bbf6f850d33272807015ed61cbdb43b3
Wesley Pereira <wesley.pereira@southsystem.com.br>
2023-02-26 12:21:56 -0300

    Microservice mail simple completed

---
+++ mail-service/cmd/api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+const webPort = "80"
+
+type Config struct {
+	Mailer Mail
+}
+
+func main() {
+	app := Config{
+		Mailer: createMail(),
+	}
+
+	log.Println("Starting mail service on port", webPort)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	s := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromAddress: os.Getenv("MAIL_ADDRESS"),
+		FromName:    os.Getenv("MAIL_NAME"),
+	}
+
+	return s
+}
